bc0e851dde240d58f9905db2860df69bbc0b0b46
Shahbaz Ahmed <ashabaz845@gmail.com>
2025-01-30 21:47:15 +0500

    Add javaScript Slide

---
+++ 30-1-25/app.js
@@ -0,0 +1,42 @@
+// const first = document.getElementById('first');
+// const second = document.getElementById("second")
+// const third = document.getElementById("third")
+
+// first.className = "color text"
+// second.className = "text color"
+
+// second.classList.add("color")
+// second.classList.remove("text")
+
+// console.log(first,second,third)
+
+const result = document.getElementById("result")
+const newElement = document.createElement("div")
+
+const text = document.createTextNode("My Name is Shahbaz")
+newElement.appendChild(text)
+
+console.log("result",result)
+
+
+// function myName(){
+//     
+
+//     console.log("username",userName.value)
+//     console.log("userEmail",userEmail.value)
+// }
+
+const userName = document.getElementById("userName")
+const userEmail = document.getElementById("userEmail")
+const form = document.getElementById("form")
+
+
+const btn = document.querySelector(".btn")
+
+form.addEventListener("submit",function(e){
+     e.preventDefault()
+    console.log("Form Submitted")
+    console.log("userName",userName.value)
+    console.log("userEmail",userEmail.value)
+})
+
