1cbf8b5d9ed9556e590f56ff60f229d20e2dac8a
Shahbaz Ahmed <ashabaz845@gmail.com>
2025-02-13 15:58:17 +0500

    Class code updated

---
+++ 13-2-25/app.js
@@ -0,0 +1,70 @@
+
+
+// var student = {
+//     name:"Shahbaz",
+//     School:"Sindh Madressatul Islam",
+//     active:true,
+//     classDays:["Monday","Tuesday"]
+// }
+
+
+
+// student.rollNo = 12345
+// student.classDays.push("Wednesday")
+// student.classDays.push("Thursday")
+
+// console.log(student.classDays[student.classDays.length-1])
+
+
+// console.log(student)
+
+
+var dataUser = {
+    UserName:"faraz",
+    education:"Completed Graduation",
+    phoneNo:1234567890,
+    email:"ashanaz@gamil.com",
+    fullName:function(){
+        return `My name is ${this.UserName}`
+    }
+    
+}
+
+console.log(fullName())
+
+
+// "use strict"
+
+// function abc(){
+//     console.log(this)
+// }
+
+
+// abc()
+
+
+
+console.log(this)
+
+
+
+// var cars = {
+//     honda:{
+//         model:{
+//             city:2019,
+//             civic:2018,
+//             color:{
+//                 red:"red",
+//             },
+//         },
+//     },
+//     alto:{
+//         model:{
+//             vxr:2019,
+//             vxl:2018
+//         }
+//     }
+// }
+
+
+
